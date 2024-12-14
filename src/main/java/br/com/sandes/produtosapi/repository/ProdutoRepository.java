305bd330480a4eba11614b90848fba0314ac611b
Mateus Sandes <mateussandes@hotmail.com.br>
2024-12-14 11:35:54 -0300

    Criado o metodo de buscar pelo nome do produto

--- src/main/java/br/com/sandes/produtosapi/repository/ProdutoRepository.java
+++ src/main/java/br/com/sandes/produtosapi/repository/ProdutoRepository.java
@@ -3,6 +3,12 @@ package br.com.sandes.produtosapi.repository;
 import br.com.sandes.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, String> { }
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
+}
